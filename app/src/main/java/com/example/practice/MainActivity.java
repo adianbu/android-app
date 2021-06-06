9c9cebbada612ec5bb0c68b03e153a02a7f048e2
adianbu <57458067+adianbu@users.noreply.github.com>
2021-06-07 00:19:36 +0530

    Toast and Text field functionality implemented

--- app/src/main/java/com/example/practice/MainActivity.java
+++ app/src/main/java/com/example/practice/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.practice;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,11 +19,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void btnClick(View v){
-
+        Context context = getApplicationContext();
        View v1 = findViewById(R.id.button11);
 
-        v.setEnabled(false);
-        Button button = (Button) v;
-        button.setText("Clicked");
+        EditText v2 = findViewById(R.id.text1);
+        String s = v2.getText().toString();
+
+        TextView v3= findViewById(R.id.view);
+        if(v3!=null)
+         v3.setText(s);
+
+        Toast.makeText(context, "Alert",Toast.LENGTH_LONG).show();
+
+//        v.setEnabled(false);
+//        Button button = (Button) v;
+//        button.setText("Clicked");
     }
 }
